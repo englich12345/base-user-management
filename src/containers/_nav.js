7a522e954c1b6d0dad5c20f16d7e4fefc2bfc907
Lich Ho (Jack) <lichars1305@gmail.com>
2021-01-10 11:50:14 +0700

    Commit init user management

---
+++ src/containers/_nav.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+
+const _nav =  [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon"/>,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    }
+  },
+  {
+    _tag: 'CSidebarNavDropdown',
+    name: 'Pages',
+    route: '/pages',
+    icon: 'cil-star',
+    _children: [
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Login',
+        to: '/login',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Register',
+        to: '/register',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Error 404',
+        to: '/404',
+      },
+      {
+        _tag: 'CSidebarNavItem',
+        name: 'Error 500',
+        to: '/500',
+      },
+    ],
+  }
+]
+
+export default _nav
